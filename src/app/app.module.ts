307a7cb62e1ab0856ed6395478b6a9e6ed3d534d
Renan Fochetto <renandfochetto@gmail.com>
2024-10-17 21:55:03 -0300

    "alterações de cores e fonte para ser acessível"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CalculadoraComponent } from './calculadora/calculadora/calculadora.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @NgModule({
   declarations: [
